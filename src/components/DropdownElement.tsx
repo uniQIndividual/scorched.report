4ac36c56c21c4e3f86d2487dac0d80d5a9b33d07
uniQ <20915306+uniQIndividual@users.noreply.github.com>
2024-12-31 18:44:22 +0100

    Fixes to the search results

--- src/components/DropdownElement.tsx
+++ src/components/DropdownElement.tsx
@@ -13,13 +13,13 @@ interface DropdownElementProps {
 export default class DropdownElement extends React.Component<DropdownElementProps> {
     override render() {
         return (
-            <a className="" href={this.props.to}>
-                <div className={"w-[276px] rounded-lg bg-[rgba(0,0,0,0.5)] hover:bg-[rgba(0,0,0,0.9)] flex backdrop-blur-[8px] text-gray-800 dark:text-white m-4 p-4 text-2xl"}>
-                    <img src={"https://www.bungie.net/" + this.props.iconPath} className="h-8 w-8 mr-4" />
-                    <div className="table">
-                        <div className="table-row-group">
-                            <div className="table-row font-bold">{this.props.username}</div>
-                            <div className="table-row text-gray-200">{this.props.subtitle}</div>
+            <a className="m-4 " href={this.props.to}>
+                <div className={"w-[276px] rounded-lg bg-[rgba(0,0,0,0.5)] hover:bg-[rgba(0,0,0,0.9)] !cursor-pointer flex backdrop-blur-[8px] text-gray-800 dark:text-white p-4 text-2xl"}>
+                    <img src={"https://www.bungie.net/" + this.props.iconPath} className="!cursor-pointer h-8 w-8 mr-4" />
+                    <div className="table !cursor-pointer">
+                        <div className="table-row-group !cursor-pointer">
+                            <div className="table-row font-bold !cursor-pointer">{this.props.username}</div>
+                            <div className="table-row text-gray-200 !cursor-pointer">{this.props.subtitle}</div>
                         </div>
                     </div>
                 </div>
