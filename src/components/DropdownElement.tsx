6865df080d26476b49f8cdb8e70f09c580632a64
uni <20915306+uniQIndividual@users.noreply.github.com>
2025-02-13 16:48:15 +0100

    new sidebar image

--- src/components/DropdownElement.tsx
+++ src/components/DropdownElement.tsx
@@ -14,11 +14,11 @@ export default class DropdownElement extends React.Component<DropdownElementProp
     override render() {
         return (
             <a className="m-4 " href={this.props.to}>
-                <div className={"w-[276px] rounded-lg bg-[rgba(0,0,0,0.5)] hover:bg-[rgba(0,0,0,0.9)] !cursor-pointer flex backdrop-blur-[8px] text-gray-800 dark:text-white p-4 text-2xl"}>
+                <div className={"w-[276px] rounded-lg bg-[rgba(0,0,0,0.5)] hover:bg-[rgba(0,0,0,0.9)] !cursor-pointer flex backdrop-blur-[8px] text-white p-4 text-2xl"}>
                     <img src={"https://www.bungie.net/" + this.props.iconPath} className="!cursor-pointer h-8 w-8 mr-4" />
-                    <div className="table !cursor-pointer">
-                        <div className="table-row-group !cursor-pointer">
-                            <div className="table-row font-bold !cursor-pointer">{this.props.username}</div>
+                    <div className="table flex-grow-0 max-w-52 text-wrap !cursor-pointer">
+                        <div className="table-row-group max-w-52 text-wrap !cursor-pointer">
+                            <div className="table-row max-w-52 text-wrap font-bold !cursor-pointer">{this.props.username}</div>
                             <div className="table-row text-gray-200 !cursor-pointer">{this.props.subtitle}</div>
                         </div>
                     </div>
