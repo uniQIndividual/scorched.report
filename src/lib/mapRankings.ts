1f57c1fb63caf7f4b3fdf4c738f4914082bf6ab4
uni <20915306+uniQIndividual@users.noreply.github.com>
2025-02-15 19:05:56 +0100

    updated map rankings

--- src/lib/mapRankings.ts
+++ src/lib/mapRankings.ts
@@ -1,110 +1,114 @@
 export const mapRankings = {
     "Altar of Flame": {
         image: "/img/destiny_content/pgcr/crucible_ness.jpg",
-        rankings: ["B", "B", "B", "S", "C", "A", "A", "A", "B", "A", "A", "B", "B", "A", "C", "B", "C", "S"]
+        rankings: ["B", "B", "B", "S", "C", "A", "A", "A", "B", "A", "A", "B", "B", "A", "C", "B", "C", "B", "C", "C", "A", "S"]
     },
     "Anomaly": {
         image: "/img/destiny_content/pgcr/crucible_the_anomaly.jpg",
-        rankings: ["C", "D", "F", "A", "S", "B", "F", "D", "F", "F", "D", "F", "F", "D", "F", "F", "F", "F"]
+        rankings: ["C", "D", "F", "A", "S", "B", "F", "D", "F", "F", "D", "F", "F", "D", "F", "F", "F", "C", "B", "F", "F", "F"]
     },
     "Bannerfall": {
         image: "/img/destiny_content/pgcr/crucible_bannerfall.jpg",
-        rankings: ["C", "A", "C", "C", "B", "B", "B", "B", "D", "B", "S", "C", "B", "B", "B", "C", "A", "D"]
+        rankings: ["C", "A", "C", "C", "B", "B", "B", "B", "D", "B", "S", "C", "B", "B", "B", "C", "A", "D", "A", "A", "C", "D"]
     },
     "Burnout": {
         image: "/img/destiny_content/pgcr/pvp_the_burnout.jpg",
-        rankings: ["B", "S", "B", "C", "F", "F", "B", "D", "D", "A", "B", "D", "C", "F", "D", "C", "D", "C"]
+        rankings: ["B", "S", "B", "C", "F", "F", "B", "D", "D", "A", "B", "D", "C", "F", "D", "C", "D", "A", "A", "C", "F", "C"]
     },
     "Cathedral of Dusk": {
         image: "/img/destiny_content/pgcr/crucible_cathedral_of_dusk.jpg",
-        rankings: ["D", "D", "D", "D", "D", "C", "F", "C", "C", "B", "B", "B", "C", "B", "F", "F", "B", "C"]
+        rankings: ["D", "D", "D", "D", "D", "C", "F", "C", "C", "B", "B", "B", "C", "B", "F", "F", "B", "A", "B", "D", "", "C"]
     },
     "Cauldron": {
         image: "/img/destiny_content/pgcr/crucible_cauldron.jpg",
-        rankings: ["C", "D", "D", "B", "A", "F", "F", "C", "C", "D", "S", "C", "D", "A", "D", "F", "D", "F"]
+        rankings: ["C", "D", "D", "B", "A", "F", "F", "C", "C", "D", "S", "C", "D", "A", "D", "F", "D", "A", "D", "F", "F", "F"]
     },
     "Cirrus Plaza": {
         image: "/img/destiny_content/pgcr/crucible_mall.jpg",
-        rankings: ["C", "A", "D", "F", "F", "F", "F", "C", "B", "D", "F", "B", "D", "B", "F", "F", "C", "D"]
+        rankings: ["C", "A", "D", "F", "F", "F", "F", "C", "B", "D", "F", "B", "D", "B", "F", "F", "C", "C", "C", "B", "", "D"]
     },
     "Convergence": {
         image: "/img/destiny_content/pgcr/pvp_longshot_2.jpg",
-        rankings: ["D", "D", "F", "D", "D", "F", "F", "B", "F", "C", "B", "F", "F", "F", "F", "F", "F", "F"]
+        rankings: ["D", "D", "F", "D", "D", "F", "F", "B", "F", "C", "B", "F", "F", "F", "F", "F", "F", "A", "F", "F", "F", "F"]
     },
     "Dead Cliffs": {
         image: "/img/destiny_content/pgcr/crucible_cliffside.jpg",
-        rankings: ["D", "A", "D", "D", "S", "A", "D", "C", "D", "C", "C", "B", "D", "B", "D", "F", "D", "D"]
+        rankings: ["D", "A", "D", "D", "S", "A", "D", "C", "D", "C", "C", "B", "D", "B", "D", "F", "D", "B", "C", "D", "F", "D"]
     },
     "Disjunction": {
         image: "/img/destiny_content/pgcr/crucible_disjunction.jpg",
-        rankings: ["S", "A", "A", "A", "A", "A", "S", "C", "B", "C", "S", "A", "S", "C", "S", "S", "B", "A"]
+        rankings: ["S", "A", "A", "A", "A", "A", "S", "C", "B", "C", "S", "A", "S", "C", "S", "S", "B", "B", "S", "S", "", "A"]
     },
     "Dissonance": {
         image: "/img/destiny_content/pgcr/crucible_root.jpg",
-        rankings: ["F", "S", "B", "C", "D", "B", "B", "F", "B", "F", "B", "C", "C", "C", "B", "D", "C", "A"]
+        rankings: ["F", "S", "B", "C", "D", "B", "B", "F", "B", "F", "B", "C", "C", "C", "B", "D", "C", "A", "B", "B", "", "A"]
     },
     "Distant Shore": {
         image: "/img/destiny_content/pgcr/crucible_distant_shore.jpg",
-        rankings: ["B", "D", "C", "A", "C", "B", "C", "A", "C", "S", "B", "C", "C", "C", "C", "D", "C", "A"]
+        rankings: ["B", "D", "C", "A", "C", "B", "F", "A", "C", "S", "B", "C", "C", "C", "C", "D", "C", "A", "D", "C", "B", "A"]
     },
     "Endless Vale": {
         image: "/img/destiny_content/pgcr/crucible_grove.jpg",
-        rankings: ["B", "S", "C", "B", "B", "C", "F", "A", "D", "S", "B", "C", "B", "B", "B", "C", "B", "B"]
+        rankings: ["B", "S", "C", "B", "B", "C", "F", "A", "D", "S", "B", "C", "B", "B", "B", "C", "B", "A", "A", "D", "C", "B"]
     },
     "Eternity": {
         image: "/img/destiny_content/pgcr/crucible_glaive.jpg",
-        rankings: ["A", "S", "S", "A", "S", "S", "S", "S", "A", "S", "S", "S", "S", "A", "S", "S", "S", "S"]
+        rankings: ["A", "S", "S", "A", "S", "S", "S", "S", "A", "S", "S", "S", "S", "A", "S", "S", "S", "A", "S", "S", "A", "S"]
     },
     "Eventide Labs": {
         image: "/img/destiny_content/pgcr/crucible_ice.jpg",
-        rankings: ["F", "D", "F", "C", "F", "A", "C", "F", "D", "D", "F", "D", "F", "D", "F", "F", "D", "F"]
+        rankings: ["F", "D", "F", "C", "F", "A", "C", "F", "D", "D", "F", "D", "F", "D", "F", "F", "D", "C", "C", "D", "", "F"]
     },
     "Exodus Blue": {
         image: "/img/destiny_content/pgcr/crucible_exodus_blue.jpg",
-        rankings: ["B", "D", "C", "A", "C", "B", "D", "B", "S", "A", "B", "B", "B", "A", "D", "B", "F", "A"]
+        rankings: ["B", "D", "C", "A", "C", "B", "D", "B", "S", "A", "B", "B", "B", "A", "D", "B", "F", "C", "B", "C", "B", "A"]
     },
     "Fortress": {
         image: "/img/destiny_content/pgcr/crucible_hull.jpg",
-        rankings: ["A", "B", "B", "A", "A", "A", "A", "B", "C", "A", "A", "A", "A", "B", "B", "A", "B", "B"]
+        rankings: ["A", "B", "B", "A", "A", "A", "A", "B", "C", "A", "A", "A", "A", "B", "B", "A", "B", "A", "A", "A", "A", "B"]
     },
     "Fragment": {
         image: "/img/destiny_content/pgcr/crucible_fragment.jpg",
-        rankings: ["B", "A", "A", "B", "C", "B", "A", "A", "S", "S", "B", "A", "A", "B", "A", "A", "A", "A"]
+        rankings: ["B", "A", "A", "B", "C", "B", "A", "A", "S", "S", "B", "A", "A", "B", "A", "A", "A", "B", "A", "A", "B", "A"]
     },
     "Javelin-4": {
         image: "/img/destiny_content/pgcr/crucible_shaft.jpg",
-        rankings: ["D", "B", "C", "B", "B", "A", "C", "C", "A", "S", "A", "S", "D", "C", "A", "D", "B", "B"]
+        rankings: ["D", "B", "C", "B", "B", "A", "C", "C", "A", "S", "A", "S", "D", "C", "A", "D", "B", "A", "B", "B", "C", "B"]
     },
     "Meltdown": {
         image: "/img/destiny_content/pgcr/crucible_meltdown.jpg",
-        rankings: ["D", "D", "D", "C", "B", "F", "F", "D", "F", "A", "F", "F", "D", "D", "D", "D", "D", "F"]
+        rankings: ["D", "D", "D", "C", "B", "F", "F", "D", "F", "A", "F", "F", "D", "D", "D", "D", "D", "D", "C", "C", "", "F"]
     },
     "Midtown": {
         image: "/img/destiny_content/pgcr/crucible_midtown.jpg",
-        rankings: ["A", "S", "C", "C", "A", "C", "B", "B", "A", "F", "B", "A", "A", "C", "A", "A", "A", "S"]
+        rankings: ["A", "S", "C", "C", "A", "C", "B", "B", "A", "F", "B", "A", "A", "C", "A", "A", "A", "A", "B", "A", "B", "S"]
     },
     "Multiplex": {
         image: "/img/destiny_content/pgcr/crucible_multiplex.jpg",
-        rankings: ["B", "C", "C", "S", "F", "B", "C", "D", "F", "F", "A", "F", "B", "C", "F", "D", "C", "C"]
+        rankings: ["B", "C", "C", "S", "F", "B", "C", "D", "F", "F", "A", "F", "B", "C", "F", "D", "C", "D", "B", "B", "", "C"]
     },
     "Radiant Cliffs": {
         image: "/img/destiny_content/pgcr/pvp_radiant_cliffs.jpg",
-        rankings: ["A", "B", "B", "A", "C", "B", "B", "A", "A", "D", "B", "A", "B", "B", "B", "C", "B", "A"]
+        rankings: ["A", "B", "B", "A", "C", "B", "B", "A", "A", "D", "B", "A", "B", "B", "B", "C", "B", "A", "B", "A", "A", "A"]
     },
     "Rusted Lands": {
         image: "/img/destiny_content/pgcr/crucible_rusted_lands.jpg",
-        rankings: ["S", "S", "S", "A", "C", "A", "S", "S", "A", "B", "B", "A", "A", "A", "A", "B", "A", "S"]
+        rankings: ["S", "S", "S", "A", "C", "A", "S", "S", "A", "B", "B", "A", "A", "A", "A", "B", "A", "B", "A", "S", "S", "S"]
+    },
+    "Solitude": {
+        image: "/img/destiny_content/pgcr/crucible_solitude.jpg",
+        rankings: ["F", "F", "C", "", "F", "F", "D", "", "", "A", "A", "", "D", "D", "", "F", "C", "C", "F", "F", "", "B"]
     },
     "Vostok": {
         image: "/img/destiny_content/pgcr/crucible_observatory.jpg",
-        rankings: ["A", "A", "A", "A", "B", "A", "A", "S", "S", "A", "A", "A", "B", "S", "A", "A", "A", "S"]
+        rankings: ["A", "A", "A", "A", "B", "A", "A", "S", "S", "A", "A", "A", "B", "S", "A", "A", "A", "S", "S", "S", "", "S"]
     },
     "Widow's Court": {
         image: "/img/destiny_content/pgcr/crucible_widows_court.jpg",
-        rankings: ["S", "A", "S", "S", "S", "S", "S", "S", "S", "A", "S", "S", "S", "S", "S", "S", "S", "S"]
+        rankings: ["S", "A", "S", "S", "S", "S", "S", "S", "S", "A", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S", "S"]
     },
     "Wormhaven": {
         image: "/img/destiny_content/pgcr/pvp_wormhaven.jpg",
-        rankings: ["C", "D", "C", "B", "D", "S", "C", "B", "B", "B", "C", "A", "C", "C", "A", "B", "C", "B"]
+        rankings: ["C", "D", "C", "B", "D", "S", "C", "B", "B", "B", "C", "A", "C", "C", "A", "B", "C", "B", "C", "C", "F", "B"]
     },
 }
