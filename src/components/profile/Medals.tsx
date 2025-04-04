1d373dc18b550e2b05167abf17118f4be05301ba
uni <20915306+uniQIndividual@users.noreply.github.com>
2025-04-04 23:33:25 +0200

    let's reverse that order

--- src/components/profile/Medals.tsx
+++ src/components/profile/Medals.tsx
@@ -8,14 +8,7 @@ export const Medals = (props: Scorcher) => {
     try {
         return (
             <div className="flex flex-wrap justify-center">
-                <D2Box title={"Info"} body={
-                    <div className="p-4">
-                        <div className="">
-                            Due to <a href="/faq#missing-data" className="underline underline-offset-2 hover:text-gray-950 hover:dark:text-gray-100">known issues with Bungie's API</a> some medals will be missing. The actual numbers should be about 5% higher.
-                        </div>
-                    </div>
-                } />
-                {[1, 2, 3].map(tier => // create a box for each tier
+                {[3, 2, 1].map(tier => // create a box for each tier
                     <D2Box title={"Tier " + tier} body={
                         <div className="" key={"_profile_showcase"}>
                             <div className="flex flex-wrap justify-center">
@@ -29,13 +22,13 @@ export const Medals = (props: Scorcher) => {
                                             <div className=" text-center align-middle ">
                                                 <span className="font-semibold text-gray-950 dark:text-white">{props.bungieHistoricMedals[medal]!.value + "x "}</span> {props.bungieHistoricMedals[medal]!.statName}
                                             </div>
-                                            <Tooltip id={medal + "_tooltip"} opacity={1} style={{ backgroundColor: "rgba(20,20,20,0.9)" }} />
                                             <div className="text-center align-middle text-sm text-gray-600 dark:text-gray-400">
                                                 {(props.bungieHistoricMedals[medal]!.value / Math.max(props.bungieHistoricAccountStats.activitiesEntered, 1)).toLocaleString(undefined, {
                                                     minimumFractionDigits: 0,
                                                     maximumFractionDigits: 2,
                                                 }) + " pga"}
                                             </div>
+                                            <Tooltip id={medal + "_tooltip"} opacity={1} style={{ backgroundColor: "rgba(20,20,20,0.9)" }} />
                                         </div>
                                         : <></>
                                 }).filter(a => a != <></>)}
@@ -43,8 +36,13 @@ export const Medals = (props: Scorcher) => {
                         </div>
                     } />
                 )}
-                <div className="flex justify-center mt-8">
-                </div>
+                <D2Box title={"Info"} body={
+                    <div className="p-4">
+                        <div className="">
+                            Due to <a href="/faq#missing-data" className="underline underline-offset-2 hover:text-gray-950 hover:dark:text-gray-100">known issues with Bungie's API</a> some medals will be missing. The actual numbers should be about 5% higher.
+                        </div>
+                    </div>
+                } />
             </div>
         )
     } catch (error) {
